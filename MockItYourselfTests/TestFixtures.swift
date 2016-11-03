f487179cd5aa93c0cc535a1e901c411537ed9809
Alexander Annas Helgason <alliannas@gmail.com>
2016-11-03 18:19:32 +0000

    Fixes String is not AnyObject in tests

--- MockItYourselfTests/TestFixtures.swift
+++ MockItYourselfTests/TestFixtures.swift
@@ -33,8 +33,8 @@ protocol ExampleProtocol {
     func methodWithArgs1(_ arg1: String)
     func methodWithArgs1ReturnsOptional(_ arg1: String) -> String?
     func methodWithArgs1Returns(_ arg1: String) -> String
-    
-    func methodWithArgs2(_ arg1: AnyObject, arg2: Selector) -> String
+
+    func methodWithArgs2(_ arg1: String, arg2: Selector?) -> String
 }
 
 class MockExampleClass: ExampleProtocol, MockItYourself {
@@ -90,7 +90,7 @@ class MockExampleClass: ExampleProtocol, MockItYourself {
         return callHandler.registerCall(args: Args1(arg(arg1)), defaultReturnValue: MockExampleClass.defaultReturnValue)
     }
     
-    func methodWithArgs2(_ arg1: AnyObject, arg2: Selector) -> String {
+    func methodWithArgs2(_ arg1: String, arg2: Selector?) -> String {
         return callHandler.registerCall(args: Args2(arg(arg1), arg(arg2)), defaultReturnValue: MockExampleClass.defaultReturnValue)
     }
     
